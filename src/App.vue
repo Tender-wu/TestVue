faa3e235791f561ac3eede719d868c8cad98d8fa
Wu Kang <776970007@qq.com>
2022-05-15 23:11:02 +0800

    第三次提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <MyButton></MyButton>
+        
+    </div>
+</template>
+
+<script>
+
+import MyButton from "./components/MyButton.vue"
+export default {
+    name:'',
+    components:{
+        MyButton
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div>
+        <h3>九九re</h3>
+        <button @click="count++">跳转备用网址{{count}}</button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:'',
+    data(){
+        return{
+            count:0
+        }
+        
+    }
+
+}
+</script>
+
+<style>
+    h3{
+        color:brown;
+    }
+</style>
